75bbe733a73f332a19831fe6c316924287101199
U202214514 <136102139+U202214514@users.noreply.github.com>
2024-12-30 20:48:37 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
